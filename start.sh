0240f7192e136a181162a116a8b582e1a75eab3e
vikawood12 <vremenno212@mail.ru>
2026-07-01 21:02:20 +0300

    Fix: полная очистка кэша при старте

--- start.sh
+++ start.sh
@@ -3,14 +3,19 @@ echo "Creating database file if not exists..."
 touch /var/www/html/database/database.sqlite
 chmod 666 /var/www/html/database/database.sqlite
 
-echo "Clearing cache..."
-php artisan config:clear
+echo "Clearing all cache..."
 php artisan route:clear
+php artisan config:clear
+php artisan cache:clear
 php artisan view:clear
 
 echo "Caching config..."
 php artisan config:cache
+
+echo "Caching routes..."
 php artisan route:cache
+
+echo "Caching views..."
 php artisan view:cache
 
 echo "Running migrations..."
